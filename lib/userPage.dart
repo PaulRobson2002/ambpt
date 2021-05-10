d0d9155f36e0a76ec15e889dbe95f3e351bbd48e
Paul <officialdrgaming@gmail.com>
2021-05-10 11:59:34 +0100

    Updayed UserPage.Dart

--- lib/userPage.dart
+++ lib/userPage.dart
@@ -13,7 +13,50 @@ class _UserPageState extends State<UserPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(globals.account.Users[globals.account.SelectedAccount].name),)
+      appBar: AppBar(
+        title: Text(
+            globals.account.Users[globals.account.SelectedAccount].name +
+                "'s Profile"),
+      ),
+      backgroundColor: Color.fromRGBO(35, 35, 35, 1),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 20.0),
+              child: Column(
+                children: <Widget>[
+                  Image(
+                  image: AssetImage("asset/images/placeholder.png"),
+                  height: MediaQuery.of(context).size.height * 0.25,
+                  ),
+                  Text(
+                    "Welcome " +
+                        globals.account.Users[globals.account.SelectedAccount]
+                            .name,
+                    style: GoogleFonts.roboto(
+                      fontStyle: FontStyle.normal,
+                      textStyle: TextStyle(
+                        color: Colors.white,
+                        fontSize: MediaQuery.of(context).size.width * 0.075,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              child: Text(
+                "Welcome " +
+                    globals.account.Users[globals.account.SelectedAccount].name,
+              ),
+              padding: EdgeInsets.only(top: 50),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
